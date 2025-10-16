4d85ed72c4233937e469cd9f944d9f3440c0feef
pauloprbs <paulorenato_bs@live.com>
2025-10-16 00:38:04 -0300

    categorização um pouco mais balanceada

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # --- Período de Análise ---
 # Altere estas datas para definir o período de tempo para todos os scripts e notebooks
-START_YEAR = 2017
+START_YEAR = 2010
 START_MONTH = 1
 END_YEAR = 2025
 END_MONTH = 10 # Outubro
