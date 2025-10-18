e39b9f0b0293c2b7a52b107900fe57ac53727472
pauloprbs <paulorenato_bs@live.com>
2025-10-17 22:55:33 -0300

    keras e config

--- config.py
+++ config.py
@@ -2,8 +2,20 @@
 from datetime import datetime
 
 # --- Período de Análise ---
-# Altere estas datas para definir o período de tempo para todos os scripts e notebooks
+# Datas iniciais fixas
 START_YEAR = 2010
 START_MONTH = 1
-END_YEAR = 2025
-END_MONTH = 10 # Outubro
+
+# --- Controle Automático ---
+# Se True -> usa o mês mais recente automaticamente
+# Se False -> usa os valores definidos manualmente em END_YEAR / END_MONTH
+USE_CURRENT_DATE = True  
+
+# Datas finais
+if USE_CURRENT_DATE:
+    today = datetime.today()
+    END_YEAR = today.year
+    END_MONTH = today.month
+else:
+    END_YEAR = 2025
+    END_MONTH = 10
